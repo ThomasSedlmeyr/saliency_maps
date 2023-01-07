5794e681adb5c2dc6dc1e81e75e2dd9feb963601
ThomasSedlmeyr <thomas.sedlmeyr@online.de>
2023-01-07 21:33:43 +0100

    First small implementations

---
+++ SaliencyMetrics.py
@@ -0,0 +1,195 @@
+import itertools
+from itertools import product
+import random
+import numpy as np
+
+from DatabaseGeneration.RoughnessTransformer import moveDomain
+
+"""
+This file contains routines for the evaluation of certain CAMs (Class Activation maps)
+"""
+
+
+def applyRandomDeletionMetricQuadraticAreaOnWholeDataSet(xData, sizeX, sizeY, valueUsedForTheDeletion):
+    resultData = np.empty(xData.shape)
+    width = xData.shape[1]
+
+    for i in range(xData.shape[0]):
+        randomPosX = random.randint(0, width - sizeX - 1)
+        randomPoxY = random.randint(0, width - sizeY - 1)
+        for x in range(sizeX):
+            for y in range(sizeY):
+                resultData[i][randomPosX + x][randomPoxY + y] = valueUsedForTheDeletion
+
+    return resultData
+
+
+def applyRandomDeletionMetricForWholeDataSetUsingOneValue(xData, numberElementsWhichShouldBeReplaced,
+                                                          valueUsedForTheReplacement):
+    resultData = np.empty(xData.shape)
+
+    for i in range(xData.shape[0]):
+        resultData[i] = applyRandomDeletionMetricForOneImageUsingOneValue(xData[i], numberElementsWhichShouldBeReplaced,
+                                                                     valueUsedForTheReplacement)
+    return resultData
+
+
+def applyRandomDeletionMetricForWholeDataSetUsingDifferentValues(xData, numberValues, valuesUsedForTheReplacement):
+    resultData = np.empty(xData.shape)
+
+    for i in range(xData.shape[0]):
+        resultData[i] = applyRandomDeletionMetricForOneImageUsingDifferentValues(xData[i], numberValues, valuesUsedForTheReplacement)
+
+    return resultData
+
+
+def applyRandomDeletionMetricForOneImageUsingOneValue(xData, numberReplacedValues, valueUsedForTheReplacement):
+    valuesUsedForTheReplacement = [valueUsedForTheReplacement] * numberReplacedValues
+    return applyRandomDeletionMetricForOneImageUsingDifferentValues(xData, numberReplacedValues, valuesUsedForTheReplacement)
+
+
+def applyRandomDeletionMetricForOneImageUsingDifferentValues(xData, numberValues, valuesUsedForTheReplacement):
+    """
+    Replaces the numberReplacedValues pixels at random positions by valuesUsedForTheReplacement
+
+    Args:
+        inputData: The input image
+        numberReplacedValues: The number values which should be replaced
+        valuesUsedForTheReplacement: The pixels which should be used for the replacement
+
+    Returns:
+        resultData: The transformed data
+    """
+
+    #For each area the salient area with the same value
+
+    resultData = np.copy(xData)
+    width = xData.shape[1]
+
+    randomTuples = random.sample(list(product(range(width), repeat=2)), k=numberValues)
+    counter = 0
+    for (x, y) in randomTuples:
+        resultData[x][y][0] = valuesUsedForTheReplacement[counter]
+        counter += 1
+
+    return resultData
+
+def replaceHighstValuesForWholeDataSetByCertainValues(xData, saliencyMaps, numberReplacedValues, valuesUsedForTheReplacement):
+    resultData = np.empty(xData.shape)
+
+    for i in range(xData.shape[0]):
+        resultData[i] = replaceHighstValuesForOneImageByCertainValues(xData[i], saliencyMaps[i], numberReplacedValues,
+                                                                      valuesUsedForTheReplacement)
+    return resultData
+
+
+def replaceHighstValuesForWholeDataSetByOnlyOneValue(xData, saliencyMaps, numberElementsWhichShouldBeReplaced,
+                                                          valueUsedForTheReplacement):
+    resultData = np.empty(xData.shape)
+
+    for i in range(xData.shape[0]):
+        resultData[i] = replaceHighstValuesForOneImageByOnlyOneValue(xData[i], saliencyMaps[i],
+                                                                     numberElementsWhichShouldBeReplaced,
+                                                                     valueUsedForTheReplacement)
+    return resultData
+
+
+def replaceHighstValuesForOneImageByOnlyOneValue(xData, saliencyMap, numberReplacedValues, valueUsedForTheReplacement):
+    valuesUsedForTheReplacement = [valueUsedForTheReplacement] * numberReplacedValues
+    return replaceHighstValuesForOneImageByCertainValues(xData, saliencyMap, numberReplacedValues, valuesUsedForTheReplacement)
+
+
+def replaceHighstValuesForOneImageByCertainValues(xData, saliencyMap, numberReplacedValues, valuesUsedForTheReplacement):
+    """
+    Replaces the numberReplacedValues highest values from the input image with the values form
+    valuesUsedForTheReplacement
+
+    Args:
+        inputData: The input image
+        numberReplacedValues: The number values which should be replaced
+        valuesUsedForTheReplacement: The pixels which should be used for the replacement
+
+    Returns:
+        resultData: The transformed data
+    """
+
+    #The data after applying the transformation
+    resultData = np.copy(xData)
+
+    flattened = saliencyMap.flatten()
+    sortedArray = np.sort(flattened)[::-1]
+    smallestValueWhichShouldBeReplaced = sortedArray[numberReplacedValues-1]
+    counter = 0
+
+    for i in range(xData.shape[0]) :
+        for j in range(xData.shape[1]):
+            if saliencyMap[i][j] >= smallestValueWhichShouldBeReplaced and counter < numberReplacedValues:
+                resultData[i][j][0] = valuesUsedForTheReplacement[counter]
+                counter += 1
+    #print("counter: " + str(counter))
+    return resultData
+
+
+def createBitMaskForSaliencyMap(xData, saliencyMap, numberReplacedValues):
+    """
+    TODO
+    """
+
+    #The data after applying the transformation
+    resultData = np.full((xData.shape), 0)
+
+    flattened = saliencyMap.flatten()
+    sortedArray = np.sort(flattened)[::-1]
+    smallestValueWhichShouldBeReplaced = sortedArray[numberReplacedValues-1]
+    counter = 0
+
+    for i in range(xData.shape[0]) :
+        for j in range(xData.shape[1]):
+            if saliencyMap[i][j] >= smallestValueWhichShouldBeReplaced and counter < numberReplacedValues:
+                resultData[i][j][0] = 1
+                counter += 1
+    #print("counter: " + str(counter))
+    return resultData
+
+
+def rotateBitMask(bitMask, degree):
+    return np.rot90(bitMask, degree)
+
+
+def flipHorizontal(bitMask):
+    return np.fliplr(bitMask)
+
+
+def flipVertical(bitMask):
+    return np.flipud(bitMask)
+
+
+def applyBitMaskToOneImage(bitMask, xData, valuesUsedForTheReplacement):
+    resultData = np.copy(xData)
+    counter = 0
+
+    for i in range(bitMask.shape[0]):
+        for j in range(bitMask.shape[1]):
+            # We only change the value where the bitmask has ones
+            if bitMask[i][j] == 1:
+                resultData[i][j] = valuesUsedForTheReplacement[counter]
+
+    return resultData
+
+
+def applyBitMaskTransformationUsingCAMforOneImage(xData, saliencyMap, bitMapTransformationFunc, numberReplacedValues,
+                                                  valuesUsedForTheReplacement):
+    bitMask = createBitMaskForSaliencyMap(xData, saliencyMap, numberReplacedValues)
+    transformedBitMask = bitMapTransformationFunc(bitMask)
+    resultData = applyBitMaskToOneImage(transformedBitMask, xData, valuesUsedForTheReplacement)
+    return resultData
+
+
+def applyBitMaskTransformationUsingCAMOnWholeDataSet(xData, saliencyMaps, bitMapTransformationFunc, numberReplacedValues,
+                                                  valuesUsedForTheReplacement):
+    resultData = np.empty(xData.shape)
+
+    for i in range(xData.shape[0]):
+        resultData[i] = applyBitMaskTransformationUsingCAMforOneImage(xData[i], saliencyMaps[i], bitMapTransformationFunc,
+                                                                      numberReplacedValues, valuesUsedForTheReplacement)
+    return resultData
---
+++ model_training.py
@@ -0,0 +1,195 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras.applications import EfficientNetB0
+import matplotlib.pyplot as plt
+import numpy as np
+from os import listdir
+import os
+from tensorflow.keras import models
+
+IMG_SIZE = 224
+
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+#gpus = tf.config.experimental.list_physical_devices('GPU')
+#for gpu in gpus:
+    #print("Der")
+#    tf.config.experimental.set_memory_growth(gpu, True)
+
+#tf.config.experimental.VirtualDeviceConfiguration(memory_limit=3000)
+#model = EfficientNetB0(weights='imagenet', drop_connect_rate=0.4, include_top=False,)
+
+img_augmentation = Sequential(
+    [
+        tf.keras.layers.experimental.preprocessing.RandomRotation(factor=0.15),
+        tf.keras.layers.experimental.preprocessing.RandomTranslation(height_factor=0.1, width_factor=0.1),
+        tf.keras.layers.experimental.preprocessing.RandomFlip(),
+        tf.keras.layers.experimental.preprocessing.RandomContrast(factor=0.1),
+    ],
+    name="img_augmentation",
+)
+def build_model(num_classes=2):
+    inputs = tf.keras.layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+    #x = img_augmentation(inputs)
+    model = EfficientNetB0(include_top=False, input_tensor=inputs, weights="imagenet")
+
+    # Freeze the pretrained weights
+    model.trainable = False
+
+    # Rebuild top
+    x = layers.GlobalAveragePooling2D(name="avg_pool")(model.output)
+    x = layers.BatchNormalization()(x)
+
+    top_dropout_rate = 0.2
+    x = layers.Dropout(top_dropout_rate, name="top_dropout")(x)
+    outputs = layers.Dense(num_classes, activation="softmax", name="pred")(x)
+
+    # Compile
+    model = tf.keras.Model(inputs, outputs, name="EfficientNet")
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2)
+    model.compile(
+        optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"],
+    )
+    return model
+
+
+
+def create_own_model(lr_rate=0.00025, num_classes=2):
+    # normLayer = Normalization()
+    # normLayer.adapt(xTrain)
+    model = models.Sequential()
+    # model.add(normLayer)
+    model.add(layers.Conv2D(64, (5, 5), activation=tf.keras.layers.LeakyReLU(alpha=0.1), input_shape=(IMG_SIZE, IMG_SIZE, 3), padding="same",name="firstConv"))
+    model.add(layers.BatchNormalization())
+    #model.add(layers.Dropout(0.3))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(64, (4, 4), activation=tf.keras.layers.LeakyReLU(alpha=0.1), input_shape=(IMG_SIZE, IMG_SIZE, 3), padding="same",name="firstConv2"))
+    model.add(layers.BatchNormalization())
+    #model.add(layers.Dropout(0.3))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    # model.add(layers.Dropout(0.4))
+    model.add(layers.Conv2D(128, (4, 4), activation=tf.keras.layers.LeakyReLU(alpha=0.1), padding="same", name="secondConv3"))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(256, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.1), padding="same", name="thirdConv"))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(512, activation=tf.keras.layers.LeakyReLU(alpha=0.1)))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Dense(num_classes, activation="softmax", name="pred"))
+
+    model.summary()
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=lr_rate), loss="categorical_crossentropy", metrics=["accuracy"],
+    )
+    return model
+
+
+def unfreeze_model(model):
+    # We unfreeze the top 20 layers while leaving BatchNorm layers frozen
+    for layer in model.layers[-20:]:
+        if not isinstance(layer, layers.BatchNormalization):
+            layer.trainable = True
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+    model.compile(
+        optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"]
+    )
+
+
+def plot_hist(hist):
+    plt.plot(hist.history["accuracy"])
+    plt.plot(hist.history["val_accuracy"])
+    plt.title("model accuracy")
+    plt.ylabel("accuracy")
+    plt.xlabel("epoch")
+    plt.legend(["train", "validation"], loc="upper left")
+    plt.show()
+
+
+def create_numpy_ds():
+    from keras.preprocessing.image import load_img
+    from keras.preprocessing.image import img_to_array
+
+    print("Servus!")
+    # define location of dataset
+    folder = '/home/thomas/Downloads/dogs-vs-cats/train/'
+    photos, labels = list(), list()
+    # enumerate files in the directory
+    counter = 0
+    for file in listdir(folder):
+        # determine class
+        output = [1.0, 0.0]
+        if file.startswith('dog'):
+            output = [0.0, 1.0]
+        # load image
+        photo = load_img(folder + file, target_size=(IMG_SIZE, IMG_SIZE))
+        # convert to numpy array
+        photo = img_to_array(photo)
+        # store
+        photos.append(photo)
+        labels.append(output)
+        if counter == 4000:
+            break
+        counter += 1
+
+    # convert to a numpy arrays
+    photos = np.asarray(photos)
+    labels = np.asarray(labels)
+    print(photos.shape, labels.shape)
+    # save the reshaped photos
+    np.save('data_set/dogs_vs_cats_photos.npy', photos)
+    np.save('data_set/dogs_vs_cats_labels.npy', labels)
+
+
+def create_train_test_numpy():
+    x = np.load("data_set/dogs_vs_cats_photos.npy")
+    y = np.load("data_set/dogs_vs_cats_labels.npy")
+
+    randomize = np.arange(len(x))
+    np.random.shuffle(randomize)
+    x = x[randomize]
+    y = y[randomize]
+
+    # use 20% for testing
+    split_index = int(x.shape[0] * 0.8)
+    np.save('data_set/x_train_small.npy', x[:split_index])
+    np.save('data_set/x_test_small.npy', x[split_index:])
+    np.save('data_set/y_train_small.npy', y[:split_index])
+    np.save('data_set/y_test_small.npy', y[split_index:])
+
+
+def read_numpy_dataSet(name_prefix, name_postfix):
+    x_train = np.load(name_prefix + 'x_train' + name_postfix)
+    x_test = np.load(name_prefix + 'x_test' + name_postfix)
+    y_train = np.load(name_prefix + 'y_train' + name_postfix)
+    y_test = np.load(name_prefix + 'y_test' + name_postfix)
+
+    return tf.convert_to_tensor(x_train), tf.convert_to_tensor(x_test), \
+        tf.convert_to_tensor(y_train), tf.convert_to_tensor(y_test)
+    #return x_train, x_test, y_train, y_test
+
+#create_numpy_ds()
+#create_train_test_numpy()
+
+x_train, x_test, y_train, y_test = read_numpy_dataSet("data_set/", "_small.npy")
+
+#unfreeze_model(model)
+
+#epochs = 10  # @param {type: "slider", min:8, max:50}
+model = build_model(2)
+#model = create_own_model()
+hist = model.fit(x=x_test, y=y_test, epochs=10, validation_split=0.1, verbose=2, batch_size=2, use_multiprocessing=True, workers=8)
+#plot_hist(hist)
